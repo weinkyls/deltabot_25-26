191ce0fa7d4b002be338af441b40d7f101113a2a
Bernd Porr <bp1@rock-5b>
2025-11-30 12:27:57 +0000

    C1 Lidar works now

--- software/rplidar_rock/printdata.cpp
+++ software/rplidar_rock/printdata.cpp
@@ -1,44 +1,31 @@
-#include "a1lidarrpi.h"
+#include "c1lidarrpi.h"
 #include <iostream>
 #include <fstream>  // Include for file handling
 #include <thread>   // For sleep functionality
 #include <chrono>   // For timing
 
 
-class DataInterface : public A1Lidar::DataInterface {
+class DataInterface : public C1Lidar::DataInterface {
 public:
-
-	void newScanAvail(float rpm, A1LidarData (&data)[A1Lidar::nDistance]) {
+	void newScanAvail(C1LidarData (&data)[C1Lidar::nDistance]) {
 		// Debugging log to see if the function is triggered
 		std::cerr << "newScanAvail triggered!" << std::endl;
-
-		std::ofstream dataFile;
-		dataFile.open("lidar_data.csv", std::ios_base::app);  // Open file in append mode
-
-		for (A1LidarData &d : data) {
+		for (C1LidarData &d : data) {
 			if (d.valid) {  // Only process valid data
-				std::cout << "x: " << d.x << "\t y: " << d.y 
-						<< "\t r: " << d.r << "\t phi: " << d.phi 
-						<< "\t strength: " << d.signal_strength << std::endl;
-
-				// Save valid data to CSV file
-				dataFile << d.x << "," << d.y << "," << d.r << "," << d.phi 
-						<< "," << d.signal_strength << "\n";
+			    std::cout << d.x << "\t" << d.y 
+				      << "\t" << d.r << "\t" << d.phi 
+				      << "\t" << d.signal_strength << std::endl;
 			}
 		}
-		dataFile.close();
 		std::cerr << ".";  // Log that data was received and saved
 	}
-
-
-
 };
 
 int main(int, char **) {
     fprintf(stderr, "Data format: x <tab> y <tab> r <tab> phi <tab> strength\n");
     fprintf(stderr, "Press any key to stop.\n");
 
-    A1Lidar lidar;
+    C1Lidar lidar;
     DataInterface dataInterface;
     lidar.registerInterface(&dataInterface);  // Registers the callback function
 
@@ -53,11 +40,7 @@ int main(int, char **) {
     // Keep the program running while scanning (waiting for new data)
     std::cerr << "Waiting for data..." << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(10));  // Allow time for scanning
-
     std::cerr << "\nStopped cleanly." << std::endl;
     lidar.stop();  // Stop the scanning and motor
     return 0;
 }
-
-
-
