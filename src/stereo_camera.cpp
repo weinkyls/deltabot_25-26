ecd681e7970646f74514139aa858acfc56605cf4
weinkyls <miaoxn.chong@gmail.com>
2026-03-13 01:41:59 +0000

    stereo_camera.cpp v1
    not tested, just getting one camera working for now

--- src/stereo_camera.cpp
+++ src/stereo_camera.cpp
@@ -1,5 +1,28 @@
-#include "stereo_camera.h"
+#include "libcam2opencv.h"
 #include <iostream>
+#include <chrono>
+
+#include "stereo_camera.h"
+
+struct camera_callback : Libcam2OpenCV::Callback {
+    virtual void hasFrame(const cv::Mat &frame, const libcamera::ControlList &) {
+        if (!frame.empty()){
+            cv::imshow("camera feed", frame);
+            cv::waitKey(1);
+        }
+    }
+};
+
 int main(void) {
+    Libcam2OpenCV camera;
+    camera_callback myCallback;
+
+    camera.registerCallback(&myCallback);
+    camera.start();
+
+    while(true){
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    
     return 0;
 }
